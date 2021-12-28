996c882f260a0a69748e5c223a39d26a800521ca
ngardam <71029615+ngardam@users.noreply.github.com>
2021-12-28 18:37:48 -0500

    Save system implemented
    Save game works in editor play mode.  Course objectives complete

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class MainManager : MonoBehaviour
     public static MainManager Instance;
 
     public string playerName;
+
+    private int sizeOfHighScoreList = 10;
 
 
     private void Awake()
@@ -31,7 +34,11 @@ public class MainManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
+        LoadGame();
+
         SceneManager.LoadScene(0); //load menu on startup
+
+
 
     }
 
@@ -44,6 +51,52 @@ public class MainManager : MonoBehaviour
     private void Update()
     {
 
+    }
+
+    private void SaveGame()
+    {
+        SaveData saveData = new SaveData();
+
+        saveData.namesArray = CreateNamesArray();
+        saveData.scoresArray = CreateScoresArray();
+
+        //saveData.highScores = highScores;
+
+        string myJson = JsonUtility.ToJson(saveData);
+
+        File.WriteAllText(Application.persistentDataPath + "/highscores.json", myJson);
+
+        Debug.Log("saving");
+        
+    }
+
+    private void LoadGame()
+    {
+        string path = Application.persistentDataPath + "/highscores.json";
+        if (File.Exists(path))
+        {
+            Debug.Log("File found");
+            string savedJson = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(savedJson);
+
+            LoadHighScoreData(data);
+
+
+        } else
+        {
+            Debug.Log("File not found");
+        }
+
+    }
+
+    private void LoadHighScoreData(SaveData data)
+    {
+        highScores.Clear();
+
+        for (int i = 0; i < data.namesArray.Length; i++)
+        {
+            highScores.Add((data.namesArray[i], data.scoresArray[i]));
+        }
     }
 
     public void SubmitScore(string name, int score)
@@ -92,6 +145,7 @@ public class MainManager : MonoBehaviour
 
     public void ExitGameButton()
     {
+        SaveGame();
         EditorApplication.ExitPlaymode(); //only works in Unity Editor
     }
 
@@ -105,9 +159,36 @@ public class MainManager : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
+    private string[] CreateNamesArray()
+    {
+        string[] _nameArray = new string[sizeOfHighScoreList];
+
+        for (int i = 0; i < highScores.Count; i++)
+        {
+            _nameArray[i] = highScores[i].Item1;
+        }
+
+        return _nameArray;
+    }
+
+    private int[] CreateScoresArray()
+    {
+        int[] _scoresArray = new int[sizeOfHighScoreList];
+
+        for (int i = 0; i < highScores.Count; i++)
+        {
+            _scoresArray[i] = highScores[i].Item2;
+        }
+
+        return _scoresArray;
+    }
+
     [serializable]
     public class SaveData
     {
-        public (string, int)[] highScores;
+        public string[] namesArray;
+        public int[] scoresArray;
+
+        //public List<(string, int)> highScores;
     }
 }
